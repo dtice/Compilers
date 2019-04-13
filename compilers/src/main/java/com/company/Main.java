fd10242618e4249ae9795ba598b61d84569c8bb8
Dillon Tice <dtice@localhost.localdomain>
2019-04-13 13:06:42 -0600

    maven support

--- compilers/src/main/java/com/company/Main.java
+++ compilers/src/main/java/com/company/Main.java
@@ -1,9 +1,9 @@
 package com.company;
 
+import java.util.Scanner;
+
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
-
-import java.util.Scanner;
 
 public class Main {
     public static void old_main(String input){
@@ -20,6 +20,7 @@ public class Main {
             Scanner in = new Scanner(System.in);
             System.out.println("What is the path to your input file? > ");
             String input = in.nextLine();
+            in.close();
             old_main(input);
         } else {
             old_main(args[0]);
